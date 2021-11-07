dc4bb6e06e6d858465d4b95a6adf4b3cd81d7396
Steven Sim <stevensim226@gmail.com>
2021-11-07 14:07:43 +0700

    Add naive search code

---
+++ NaiveSearch.java
@@ -0,0 +1,29 @@
+public class NaiveSearch {
+    public static int naiveSearch(String pattern,String target,int m,int n) {
+        int t = 0;
+        while (t <= n - m) {
+            int p2 = 0;
+            int t2 = t;
+            while (p2 < m && pattern.charAt(p2) == target.charAt(t2)) {
+                p2++;
+                t2++;
+            }
+            if (p2 >= m) {
+                return t2 - m;
+            }
+            t++;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String pattern = "xxxy";
+        String target  = "xxxxxxxxxy";
+        int m = pattern.length();
+        int n = target.length();
+
+        // Naive search starts here
+        System.out.println(naiveSearch(pattern, target, m, n));
+        
+    }
+}
